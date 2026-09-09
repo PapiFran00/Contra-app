ed28f5c587f7b245738c2d9e37480d2799951b03
Francisco Aguirre <aguirrefco2004@gmail.com>
2026-09-09 19:34:52 -0300

    Fix: limpiar NuGet.Config para Docker

--- obj/Debug/net8.0/ContraApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContraApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContraApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7421eb21933e5ca80ee2d3b5011f6f0f96da0f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContraApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContraApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
